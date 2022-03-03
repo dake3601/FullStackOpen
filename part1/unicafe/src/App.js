7afb0fde8a868853a3de50b46684d4508e4bfbe2
Keven Arroyo <dake.3601@gmail.com>
2022-03-02 19:34:13 -0600

    Part 1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -14,6 +14,8 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const total = good + neutral + bad
+
   return (
     <div>
       <h1>give feedback</h1>
@@ -24,6 +26,9 @@ const App = () => {
       <p>good {good} </p>
       <p>neutral {neutral} </p>
       <p>bad {bad} </p>
+      <p>all {good-bad} </p>
+      <p>average {(good-bad)/total || 0} </p>
+      <p>positive {100 * good/total || 0} % </p>
     </div>
   )
 }
