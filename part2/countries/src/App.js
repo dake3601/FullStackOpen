27050f706eb6e1114ec7309cc7035e8cad2071a9
Keven Arroyo <dake.3601@gmail.com>
2022-03-05 12:17:01 -0600

    Part 2.12

---
+++ part2/countries/src/App.js
@@ -0,0 +1,83 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
+const Filter = ({ filter, handleFilterChange}) => (
+  <form>
+    <div>
+      find countries: <input 
+              value={filter}
+              onChange={handleFilterChange}
+            />
+    </div>
+  </form>
+)
+
+const Countries = ({ countriesToShow }) => {
+  if (countriesToShow.length > 10) {
+    return (
+      <p>Too many matches, specify another filter</p>
+    )
+  } else if (countriesToShow.length === 1) {
+    const country = countriesToShow[0]
+    const languages = Object.values(country.languages)
+    const src = country.flags.png
+    return (
+      <div>
+        <h2>{country.name.common}</h2>
+        <p>capital {country.capital[0]}</p>
+        <p>area {country.area}</p>
+        <h3>languages:</h3>
+        <ul>
+          {languages.map(lang => 
+            <li key={lang}>{lang}</li>)}
+        </ul>
+        <img src={src} alt={`Flag of ${country.name.common}`}/>
+      </div>
+    )
+  }
+  return (
+    <ul>
+      {countriesToShow.map(({ name }) => <li key={name.common}>{name.common}</li>)}
+    </ul>
+  )
+}
+
+const App = () => {
+  const [countries, setCountries] = useState([])
+  const [filter, setNewFilter] = useState('');
+
+  useEffect(() => {
+    axios
+      .get('https://restcountries.com/v3.1/all')
+      .then(response => {
+        setCountries(response.data)
+      })
+  }, [])
+
+  const handleFilterChange = (event) => {
+    setNewFilter(event.target.value)
+  }
+
+  const countriesToShow = filter === '' 
+                        ? countries 
+                        : countries.filter(({ name }) => 
+                            name.common.toLowerCase().includes(filter.toLowerCase()))
+
+  return (
+    <div>
+      <h1>Countries</h1>
+      <Filter 
+        filter={filter} 
+        handleFilterChange={handleFilterChange}
+
+      />
+      { filter !== '' &&
+        <Countries 
+          countriesToShow={countriesToShow} 
+        />
+      }
+    </div>
+  )
+}
+
+export default App
