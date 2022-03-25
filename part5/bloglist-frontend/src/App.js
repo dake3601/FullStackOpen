b63b8491b79d5b3b3b6b83e28bc9326098873ead
Keven Arroyo <dake.3601@gmail.com>
2022-03-25 17:23:24 -0600

    Part 5.2

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -15,6 +15,14 @@ const App = () => {
     )  
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+    }
+  }, [])
+
   const handleLogin = async (event) => {
     event.preventDefault()
     
@@ -23,11 +31,19 @@ const App = () => {
         username, password,
       })
       setUser(user)
+      window.localStorage.setItem(
+        'loggedBlogappUser', JSON.stringify(user)
+      ) 
       setUsername('')
       setPassword('')
     } catch (exception) {
       console.log('wrong username or password')
     }
+  }
+
+  const handleLogout = () => {
+    setUser(null)
+    window.localStorage.removeItem('loggedBlogappUser') 
   }
 
   if (user === null) {
@@ -62,9 +78,14 @@ const App = () => {
   return (
     <div>
       <h2>blogs</h2>
-      <p> 
-        {user.name} logged in {'\n'}
-      </p>
+      <div>
+        <p> 
+          {user.name} logged in {'\n'}
+          <button onClick={handleLogout}>
+            logout
+          </button>
+        </p>
+      </div>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
