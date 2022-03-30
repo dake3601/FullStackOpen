e797d30c08ca403a0bff39840202ead9cf82ed88
Keven Arroyo <dake.3601@gmail.com>
2022-03-29 22:18:58 -0600

    Part 5.14

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
-test('renders content', () => {
+describe('<Blog />', () => {
+  let container
+
   const blog = {
     title: 'A Guide on React',
     author: 'First Last',
@@ -11,19 +14,46 @@ test('renders content', () => {
     likes: 10
   }
 
-  const { container } = render(<Blog blog={blog} />)
+  beforeEach(() => {
+    container = render(
+      <Blog
+        blog={blog}
+      />
+    ).container
+  })
 
-  const divShow = container.querySelector('.blogShow')
-  expect(divShow).toHaveStyle('display: none')
-  expect(divShow).toHaveTextContent('A Guide on React')
-  expect(divShow).toHaveTextContent('First Last')
-  expect(divShow).toHaveTextContent('https://reactjs.org/')
-  expect(divShow).toHaveTextContent('10')
+  test('at start render title and author only', () => {
+    const divShow = container.querySelector('.blogShow')
+    expect(divShow).toHaveStyle('display: none')
+    expect(divShow).toHaveTextContent('A Guide on React')
+    expect(divShow).toHaveTextContent('First Last')
+    expect(divShow).toHaveTextContent('https://reactjs.org/')
+    expect(divShow).toHaveTextContent('10')
 
-  const divHide = container.querySelector('.blogHide')
-  expect(divHide).toHaveStyle('display: block')
-  expect(divHide).toHaveTextContent('A Guide on React')
-  expect(divHide).toHaveTextContent('First Last')
-  expect(divHide).not.toHaveTextContent('https://reactjs.org/')
-  expect(divHide).not.toHaveTextContent('10')
+    const divHide = container.querySelector('.blogHide')
+    expect(divHide).not.toHaveStyle('display: none')
+    expect(divHide).toHaveTextContent('A Guide on React')
+    expect(divHide).toHaveTextContent('First Last')
+    expect(divHide).not.toHaveTextContent('https://reactjs.org/')
+    expect(divHide).not.toHaveTextContent('10')
+  })
+
+  test('after clicking the button, url and likes are displayed', () => {
+    const button = screen.getByText('view')
+    userEvent.click(button)
+
+    const divShow = container.querySelector('.blogShow')
+    expect(divShow).not.toHaveStyle('display: none')
+    expect(divShow).toHaveTextContent('A Guide on React')
+    expect(divShow).toHaveTextContent('First Last')
+    expect(divShow).toHaveTextContent('https://reactjs.org/')
+    expect(divShow).toHaveTextContent('10')
+
+    const divHide = container.querySelector('.blogHide')
+    expect(divHide).toHaveStyle('display: none')
+    expect(divHide).toHaveTextContent('A Guide on React')
+    expect(divHide).toHaveTextContent('First Last')
+    expect(divHide).not.toHaveTextContent('https://reactjs.org/')
+    expect(divHide).not.toHaveTextContent('10')
+  })
 })
