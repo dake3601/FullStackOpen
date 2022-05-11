1175d7062a019eeac033eac331cdcd16ed155a06
Keven Arroyo <dake.3601@gmail.com>
2022-05-11 18:16:54 -0500

    Part 8.9

--- part8/library-frontend/src/components/Books.js
+++ part8/library-frontend/src/components/Books.js
@@ -1,9 +1,24 @@
+import { useQuery } from '@apollo/client'
+
+import { ALL_BOOKS } from '../queries'
+
 const Books = (props) => {
+  const result = useQuery(ALL_BOOKS)
+
   if (!props.show) {
     return null
   }
 
-  const books = []
+  if (result.loading) {
+    return (
+      <div>
+        <h2>books</h2>
+        loading...
+      </div>
+    )
+  }
+
+  const books = result.data.allBooks
 
   return (
     <div>
